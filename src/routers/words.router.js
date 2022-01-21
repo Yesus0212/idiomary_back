04bed5d9af9d8417cacd3254e64ea0f308853455
Yesus0212 <jesussolispadron@gmail.com>
2022-01-20 20:59:00 -0600

    no

--- src/routers/words.router.js
+++ src/routers/words.router.js
@@ -9,6 +9,6 @@ router.post('/', wordController.setWord);
 router.delete('/:id', wordController.deleteWord);
 router.patch('/newItem/:id', wordController.setNewItemWord);
 router.patch('/updateStatus/:id', wordController.updateStatusWord);
-router.get('/filters/', wordController.getFilters);
+router.get('/', wordController.getFilters);
 
 module.exports = router;
