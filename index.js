3b17c1eb7881ca836f1d706585d6c31592b98337
Yesus0212 <jesussolispadron@gmail.com>
2022-02-09 14:03:13 -0600

    Iniciando configuracion de SocketIO

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ require('dotenv').config();
 const dbConnect = require('./src/lib/db');
 const server = require('./src/lib/server');
 
+const http = require("http");
+const socketIo = require("socket.io");
+
 dbConnect(process.env)
     .then(() => {
         console.log('DB Connected!');
@@ -10,6 +13,11 @@ dbConnect(process.env)
         server.listen(process.env.PORT, () => {
             console.log('Server is running');
         });
+
+        const socketServer = http.createServer(server);
+        const io = socketIo(socketServer);
+        const getApiAndEmit = "TODO";
+
     })
     .catch((error) => {
         console.error(error);
